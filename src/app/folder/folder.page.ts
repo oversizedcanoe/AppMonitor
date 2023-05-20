2114b140077aa54dd2580a3644579857a8fd972a
Billy Cook <billy.cook12345@gmail.com>
2023-05-20 17:49:17 -0400

    Initial commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {AppList, AppInfo} from 'app-list-capacitor'
 
 @Component({
   selector: 'app-folder',
@@ -13,5 +14,23 @@ export class FolderPage implements OnInit {
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  async thing(){
+    const res = await AppList.getInstalledApps();
+
+    let names: string[] = [];
+
+    let appList: AppInfo[] = res.installedApps;
+
+    console.log("TEST123", typeof(appList))
+    console.log("TEST456", JSON.stringify(appList))
+
+    for (let index = 0; index < appList.length; index++) {
+      const element = appList[index];
+      names.push(element.name)
+    }
+
+    alert(names.join('\n'));
   }
 }
