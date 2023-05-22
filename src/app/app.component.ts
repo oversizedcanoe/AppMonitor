9c5a246bf9fc4e635a9e82313625fc7e84fc2000
Billy Cook <billy.cook12345@gmail.com>
2023-05-21 22:04:51 -0400

    Displayed apps in list for testing purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppInfo, AppList } from 'app-list-capacitor';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  apps: AppInfo[] = [];
+  
   constructor() {
     
+  }
+
+  async ngOnInit() {
+    let allApps = (await AppList.getInstalledApps()).installedApps;
+    allApps.sort((app1, app2) => app1.name.localeCompare(app2.name));
+    // We only care about non-system apps or system apps which have been updated (pre-installed, such as GMail)
+    this.apps = allApps.filter((app) => app.isSystemApp == false || app.isUpdatedSystemApp == true);
   }
 }
